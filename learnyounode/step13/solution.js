823341de6d2be7bac02479a6e2f680e7c220312d
Sam Clark <samrocksc@gmail.com>
2015-07-27 17:49:54 -0700

    bam fuck yeah
    bam!

--- learnyounode/step13/solution.js
+++ learnyounode/step13/solution.js
@@ -11,30 +11,30 @@ var routes = {
             minute: d.getMinutes(),
             second: d.getSeconds()
         }
-    }
+    },
     //second endpoint for /api/unixtime returning a json object
     '/api/unixtime': function(parsedURL) {
         return {
             unixtime: (new Date(parsedUrl.query.iso)).getTime()
-        };
+        }
     }
-}
 }
 
 //write an http server that servs JSON when it receives a GET 
 //request.
 server = http.createServer(function(req, res) {
-	parsedUrl = url.parse(req.url, true);
-	routing = routes[parsedUrl.pathname];
-	if(routing) {
-		res.writeHead(200, { 'Content-Type': 'application/json' })
-		response.end(JSON.stringfy(res(parsedUrl)))
-	}
-	else {
-		response.writeHead(404);
-		response.end()
-	}
+    parsedUrl = url.parse(req.url, true);
+    routing = routes[parsedUrl.pathname];
+    if (routing) {
+        res.writeHead(200, {
+            'Content-Type': 'application/json'
+        });
+        res.end(JSON.stringify(routing(parsedUrl)));
+    } else {
+        res.writeHead(404);
+        res.end();
+    }
 })
-server.listen(process.argv[2], function(){
-	console.log('listening on port' + process.argv[2])
+server.listen(process.argv[2], function() {
+    console.log('listening on port' + process.argv[2])
 })
